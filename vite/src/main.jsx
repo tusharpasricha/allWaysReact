0a1518a17bd71f8f1693056e51402200e6fbdb45
Tushar Pasricha <tusharpasricha@Tushars-MacBook-Air-2.local>
2023-10-07 18:50:57 +0530

    custom elements on vite

--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -1,10 +1,38 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function myApp(){
+  return(
+    <>
+    <div>hi</div>
+    </>
+  )
+}
+
+const myElement = (<><div>myElement</div></>)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com'},
+  "reactElement"
+  )
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //myApp()
+  //myElement
+  reactElement
+
 )
